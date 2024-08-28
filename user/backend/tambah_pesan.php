f2f6b545482bba933993785a0f3820768cbe936c
GalihFauzan <galihfauzan01@gmail.com>
2024-08-28 16:10:37 +0700

    fix tambah_pesan

--- user/backend/tambah_pesan.php
+++ user/backend/tambah_pesan.php
@@ -7,5 +7,5 @@ $id_kamar = $_POST['id_kamar'];
 $judul = $_POST['judul'];
 $pesan = $_POST['pesan'];
 
-$query = mysqli_query($conn, "INSERT INTO pesan values('','$id_user','$tgl_pesan','$id_kamar','$judul','$pesan','Menunggu Konfirmasi')");
+$query = mysqli_query($conn, "INSERT INTO (id_user,tgl_pesan,id_kamar,judul,pesan,Menunggu Konfirmasipesan) values('$id_user','$tgl_pesan','$id_kamar','$judul','$pesan','Menunggu Konfirmasi')");
 header("location:../index.php?page=4");
